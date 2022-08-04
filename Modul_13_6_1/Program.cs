1efe6538b112f9251577670d9f2feeb28bcdcb83
DimVinich <DmitrijVS@gmail.com>
2022-08-04 21:29:36 +0300

    К проверке

--- Modul_13_6_1/Program.cs
+++ Modul_13_6_1/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-
+using System.IO;
 
 namespace Modul_13_6_1
 {
@@ -10,21 +10,51 @@ namespace Modul_13_6_1
         static void Main(string[] args)
         {
 
-            //  считать файл
+            // Считываем файл в массив, с него будем рабоать с коллекциями
+            // Не будем произоводить очистку памяти,  считаем
+            // что памяти системного блока достаточно для обеих коллекций
+
+            string nFile = @"D:\\Text1.txt";
+            string[] arrText;
 
 
-            //  Запустить стовпвотч
-            var watchTwo = Stopwatch.StartNew();
+            List<string> lList = new List<string>();
+            LinkedList<string> lLinkedList = new LinkedList<string>();
+            int iCount = 0;
 
-            //  Закинуть в Лист
+            try
+            {
+                arrText = File.ReadAllLines(nFile);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("При считывании файла случились проблемы \n" +ex.Message );
+                Console.ReadKey();
+                return;
+            }
 
-            //  Выдать стопвотч
-            Console.WriteLine($"Вставка в  словарь: {watchTwo.Elapsed.TotalMilliseconds}  мс");
+            //  Обработка связанного списка
+            iCount = 0;
+            var watchLinckedList = Stopwatch.StartNew();
 
-            //  Запустить стоп вотч
-            //  Закинуть в Связанный Лист
-            //  Выдать стопвотч
+            foreach (string lsTemp in arrText)
+            {
+                iCount++;
+                lLinkedList.AddLast(lsTemp);
+            }
 
+            Console.WriteLine($"Заполнение связанного списка заняло: {watchLinckedList.Elapsed.TotalMilliseconds}  мс  для {iCount} записей");
+
+            //  Обработка для списка
+            var watchList = Stopwatch.StartNew();
+
+            foreach(string lsTemp in arrText)
+            {
+                iCount++;
+                lList.Add(lsTemp);
+            }
+
+            Console.WriteLine($"Заполнение списка заняло: {watchList.Elapsed.TotalMilliseconds}  мс  для {iCount} записей");
 
 
             Console.ReadKey();
